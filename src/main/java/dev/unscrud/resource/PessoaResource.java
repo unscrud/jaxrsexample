4c6c42596534be7559a3da844da28b6dc6020bc9
Daniel Carvalho <daniel.devop@gmail.com>
2022-07-16 17:57:07 -0300

    listar pessoas

--- src/main/java/dev/unscrud/resource/PessoaResource.java
+++ src/main/java/dev/unscrud/resource/PessoaResource.java
@@ -3,16 +3,24 @@ package dev.unscrud.resource;
 import dev.unscrud.model.Pessoa;
 import dev.unscrud.repository.PessoaRepository;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import java.util.List;
 
 @Path("/pessoa")
 public class PessoaResource {
   private PessoaRepository repository = new PessoaRepository();
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public List<Pessoa> get() {
+    return repository.getAll();
+  }
 
   @PUT
   @Path("{id}")
@@ -28,7 +36,8 @@ public class PessoaResource {
       repository.edit(pessoa);
       return Response.status(Response.Status.OK).entity(pessoa).build();
     } catch (Exception e) {
-      return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+      return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+          .entity(e.getMessage()).build();
     }
   }
 }
